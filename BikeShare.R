64b07ce25014627cad7d02c0909c49180c09a39b
Liz Mendoza <lizeth.mendoza0105@gmail.com>
2025-09-08 13:32:35 -0600

    Update BikeShare.R

--- BikeShare.R
+++ BikeShare.R
@@ -1,1 +1,31 @@
 library(tidyverse)
+library(vroom)
+library(patchwork)
+
+bikeshare <- vroom("train.csv")
+
+dplyr::glimpse(bikeshare)
+skimr::skim(bikeshare)
+DataExplorer::plot_intro(bikeshare)
+DataExplorer::plot_correlation(bikeshare)
+DataExplorer::plot_bar(bikeshare)
+DataExplorer::plot_histogram(bikeshare)
+
+
+casual_v_registered <- ggplot(data=bikeshare, aes(x = casual, y = registered)) +
+  geom_point() + geom_smooth(se = FALSE)
+
+bar_weather <- ggplot(data = bikeshare, aes(x = weather, fill = factor(weather))) +
+  geom_bar()
+
+season_boxplot <- ggplot(data = bikeshare, aes(x = count, 
+                                               y = humidity, 
+                                               group = season,
+                                               fill = factor(season))) +
+  geom_boxplot()
+
+temp_v_count <- ggplot(data=bikeshare, aes(x = temp, y = count)) +
+  geom_point() + geom_smooth(se = FALSE)
+
+(casual_v_registered + bar_weather) / (season_boxplot + temp_v_count)
+
