5cb392edd399a250ab39c7d90ef3e4a60947f7b7
Liz Mendoza <lizeth.mendoza0105@gmail.com>
2025-09-12 16:15:51 -0600

    finished the workflow

--- BikeShare.R
+++ BikeShare.R
@@ -1,16 +1,9 @@
 library(tidyverse)
 library(vroom)
 library(patchwork)
+library(tidymodels)
 
-bikeshare <- vroom("train.csv")
-
-dplyr::glimpse(bikeshare)
-skimr::skim(bikeshare)
-DataExplorer::plot_intro(bikeshare)
-DataExplorer::plot_correlation(bikeshare)
-DataExplorer::plot_bar(bikeshare)
-DataExplorer::plot_histogram(bikeshare)
-
+train_bike <- vroom("train.csv")
 
 casual_v_registered <- ggplot(data=bikeshare, aes(x = casual, y = registered)) +
   geom_point() + geom_smooth(se = FALSE)
@@ -31,21 +24,33 @@ temp_v_count <- ggplot(data=bikeshare, aes(x = temp, y = count)) +
 
 
 
-
-## linear regression
-library(tidymodels)
-
-train_bike <- vroom("train.csv")
+# CLEANING / FEATURE ENGINEERING
 train_bike <- train_bike[, -c(10,11)]
-test_bike <- vroom("test.csv")
-#test_bike <- test_bike[,-c(1)]
+train_bike$count <- log(train_bike$count)
+
+bike_recipe <- recipe(count ~., data = train_bike) %>% 
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>%
+  step_mutate(weather = factor(weather)) %>%
+  step_time(datetime, features = "hour") %>%
+  step_mutate(season = factor(season)) %>%
+  step_date(datetime, features = "dow")
+
+
+# LINEAR REGRESSION WORKFLOW
 
 my_linear_model <- linear_reg() %>% 
   set_engine("lm") %>% 
-  set_mode("regression") %>%
-  fit(formula = count~., data = train_bike)
+  set_mode("regression")
 
-predictions <- predict(my_linear_model, new_data = test_bike)
+bike_workflow <- workflow() %>% 
+  add_recipe(bike_recipe) %>%
+  add_model(my_linear_model) %>%
+  fit(data = train_bike)
+
+test_bike <- vroom("test.csv")
+predictions <- exp(predict(bike_workflow, new_data = test_bike))
+
+# predictions <- predict(my_linear_model, new_data = test_bike)
 
 output <- test_bike %>% 
   select(datetime) %>%
@@ -53,8 +58,8 @@ output <- test_bike %>%
   rename(count = .pred) %>%
   mutate(datetime = as.character(format(datetime)))
 
-output$count <- ifelse(output$count < 0, 0, output$count)
+# output$count <- ifelse(output$count < 0, 0, output$count)
 
-vroom_write(output, "bike_predictions.csv", delim = ',')
+vroom_write(output, "bike_predictions2.csv", delim = ',')
 
 
